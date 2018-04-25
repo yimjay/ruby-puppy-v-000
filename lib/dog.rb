42258cdee4ff06e0a13d3d3332744fff8f391023
Ji Yim <gsooyim@gmail.com>
2018-04-25 23:29:08 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  attr_accessor :name, 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  
+end
